878b9eaff06a98b3e1a56061e38dae791134d5a2
bmotta1506 <bmotta1506@gmail.com>
2025-11-26 14:41:46 -0500

    Ejercicio 03

---
+++ BRAYANM/Laboratorio18/Ejercicio03/Ejercicio03.js
@@ -0,0 +1,10 @@
+function cargarUsuario() {
+    fetch("https://jsonplaceholder.typicode.com/users/10")
+        .then(response => response.json())
+        .then(data => {
+            console.log("Nombre: ", data.name),
+            console.log("Email: ", data.email),
+            console.log("Username: ", data.username)
+        })
+    .catch(error => console.error("Error: ", error));
+}
